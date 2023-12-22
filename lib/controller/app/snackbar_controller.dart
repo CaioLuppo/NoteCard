1ce101a6f0d7d930c26825bafc255010ef15995b
CaioLuppo <luppocode@outlook.com>
2023-12-22 00:14:04 -0300

    style: snackbar

--- lib/controller/app/snackbar_controller.dart
+++ lib/controller/app/snackbar_controller.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:note_card/view/resources/strings.dart';
 
 void showErrorSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       backgroundColor: Colors.red,
+      behavior: SnackBarBehavior.floating,
+      action: SnackBarAction(
+        label: snackBarActionString,
+        onPressed: () => ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+        textColor: Colors.yellow[300],
+      ),
       content: Text(
         message,
         style: const TextStyle(
