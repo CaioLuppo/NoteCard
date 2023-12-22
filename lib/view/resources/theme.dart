f14149ac9612e9c61d9fd805358ac98073cbd2a4
CaioLuppo <luppocode@outlook.com>
2023-12-21 22:12:00 -0300

    style: tema

---
+++ lib/view/resources/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Constants
+const backgroundBoxDecoration = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      Color(0xFF204d5f),
+      Color(0xFF319b85),
+    ],
+  ),
+);
+
