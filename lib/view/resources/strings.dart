1832248d6cc60ee3246d1c4fd5e331931135fc9c
CaioLuppo <luppocode@outlook.com>
2023-12-21 22:11:49 -0300

    refact: arquivo de constantes string

---
+++ lib/view/resources/strings.dart
@@ -0,0 +1,5 @@
+const userLabelText = "Usuário";
+const passwordLabelText = "Senha";
+const privacyText = "Política de privacidade";
+const loginButtonLabel = "Entrar";
+const emptyMessage = "Ainda não há notas!\nDigite algo e pressione enter.";
