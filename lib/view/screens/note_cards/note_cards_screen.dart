742390d01199a387716f1bc5bbe8775714a2eca3
CaioLuppo <luppocode@outlook.com>
2023-12-21 22:12:42 -0300

    refact: remoção de import desnecessário

--- lib/view/screens/note_cards/note_cards_screen.dart
+++ lib/view/screens/note_cards/note_cards_screen.dart
@@ -2,16 +2,15 @@ library note_cards;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:note_card/controller/app/snackbar_controller.dart';
 import 'package:note_card/controller/cards/note_card_controller.dart';
 import 'package:note_card/controller/cards/text_input_controller.dart';
 import 'package:note_card/model/store/cards_store.dart';
 import 'package:note_card/view/resources/strings.dart';
 
-part 'components/note_card.dart';
 part 'components/card_list.dart';
 part 'components/cards_session.dart';
 part 'components/empty_message.dart';
+part 'components/note_card.dart';
 part 'components/text_input.dart';
 
 class NoteCardsScreen extends StatelessWidget {
